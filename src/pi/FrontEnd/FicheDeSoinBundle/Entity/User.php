43b7040cd198c705894142a4782778bb53bcb8e4
Makni Ahmed <makni.ahmed@esprit.tn>
2018-03-21 18:53:45 +0100

    fosuser

---
+++ src/pi/FrontEnd/FicheDeSoinBundle/Entity/User.php
@@ -0,0 +1,31 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: pc asus
+ * Date: 21/03/2018
+ * Time: 18:20
+ */
+
+namespace pi\FrontEnd\FicheDeSoinBundle\Entity;
+use FOS\UserBundle\Model\User as BaseUser;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity()
+ * @ORM\Table(name="fos_user")
+ */
+class User extends BaseUser
+{
+    /**
+     * @ORM\Id
+     * @ORM\Column(type="integer")
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    protected $id;
+
+    public function __construct()
+    {
+        parent::__construct();
+        // your own logic
+    }
+}
