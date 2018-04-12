46b281f0233ce80e195ccd517e4723481514a686
GSalah <salah4991@hotmail.com>
2018-04-12 03:39:59 +0100

    lskhdghokm

--- src/pi/FrontEnd/FicheDeSoinBundle/Controller/DefaultController.php
+++ src/pi/FrontEnd/FicheDeSoinBundle/Controller/DefaultController.php
@@ -11,12 +11,9 @@ class DefaultController extends Controller
         return $this->render(':FrontEnd:layout.html.twig');
     }
 
-<<<<<<< HEAD
 
-=======
     public function notActivedAction()
     {
         return $this->render('@FicheDeSoin/f_soin/404notFound.html.twig');
     }
->>>>>>> aecd55095bfd9a8ec3d097f9c8ac1652a7938404
 }
